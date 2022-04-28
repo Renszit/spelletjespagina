5c2493e7d8a20a0a908a4a97d6639d65814ec6a2
Eileen <eileen@teech.de>
2022-04-28 18:00:27 +0200

    Vakjes goed

--- components/lingo/Word.vue
+++ components/lingo/Word.vue
@@ -1,10 +1,10 @@
 <template lang="">
   <div class="flex-1 flex bg-red-100">
-    <div class="w-1/5 h-1/5 border-black border-2"></div>
-    <div class="w-1/5 h-1/5 border-black border-2"></div>
-    <div class="w-1/5 h-1/5 border-black border-2"></div>
-    <div class="w-1/5 h-1/5 border-black border-2"></div>
-    <div class="w-1/5 h-1/5 border-black border-2"></div>
+    <div class="w-1/5 h-full border-black border-2"></div>
+    <div class="w-1/5 h-full border-black border-2"></div>
+    <div class="w-1/5 h-full border-black border-2"></div>
+    <div class="w-1/5 h-full border-black border-2"></div>
+    <div class="w-1/5 h-full border-black border-2"></div>
   </div>
 </template>
 
