5d06a3801b07ba7162bcd783d73e147345d9322d
Eileen <eileen@teech.de>
2022-05-02 18:01:20 +0200

    klein begin gemaakt en een paar styles toegevoegd

--- components/lingo/LingoBoard.vue
+++ components/lingo/LingoBoard.vue
@@ -1,29 +1,41 @@
 <template>
-  <div class="w-full h-full">
-    <p>Lingo Bord {{ this.word }}</p>
-    <div class="border-2 border-black w-4/5 h-3/4 flex flex-col">
-      <Word :word="this.word" />
-      <Word :word="this.word" />
-      <Word :word="this.word" />
-      <Word :word="this.word" />
-      <Word :word="this.word" />
+    <div class="w-full h-full">
+        <p class="mb-10 flex justify-center text-xl font-semibold w-full">
+            Lingo Bord {{ this.word }}
+        </p>
+        <div class="border-2 border-black mx-auto w-4/5 h-3/4 flex flex-col">
+            <Word :word="this.word" />
+            <Word :word="this.word" />
+            <Word :word="this.word" />
+            <Word :word="this.word" />
+            <Word :word="this.word" />
+        </div>
+
+        <div class="mt-10 w-full space-x-3 flex justify-center">
+            <form action="">
+                <input class="rounded-md" maxLength="5" type="text" />
+                <input value="Submit"
+                    type="submit"
+                    class="border border-black px-3 rounded-md"
+                />
+            </form>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import Word from "./Word.vue";
+import Word from './Word.vue'
 export default {
-  name: "LingoBoard",
-  data() {
-    return {
-      word: "lezen",
-    };
-  },
-  components: {
-    Word,
-  },
-};
+    name: 'LingoBoard',
+    data() {
+        return {
+            word: 'lezen',
+        }
+    },
+    components: {
+        Word,
+    },
+}
 // 5 rijen
 // 5 kolommen
 // 5 letters
--- components/lingo/Word.vue
+++ components/lingo/Word.vue
@@ -1,21 +1,31 @@
 <template lang="">
-  <div class="flex-1 flex bg-red-100">
-    <div class="w-1/5 h-full border-black border-2">
-      <p>{{ this.letters[0] }}</p>
-    </div>
-    <div class="w-1/5 h-full border-black border-2">
-      <p>{{ this.letters[1] }}</p>
-    </div>
-    <div class="w-1/5 h-full border-black border-2">
-      <p>{{ this.letters[2] }}</p>
-    </div>
-    <div class="w-1/5 h-full border-black border-2">
-      <p>{{ this.letters[3] }}</p>
-    </div>
-    <div class="w-1/5 h-full border-black border-2">
-      <p>{{ this.letters[4] }}</p>
-    </div>
-  </div>
+	<div class="flex-1 flex bg-red-100">
+		<div class="w-1/5 h-full border-black border-2">
+			<p class="text-3xl flex justify-center items-center w-full h-full">
+				{{ this.letters[0] }}
+			</p>
+		</div>
+		<div class="w-1/5 h-full border-black border-2">
+			<p class="text-3xl flex justify-center items-center w-full h-full">
+				{{ this.letters[1] }}
+			</p>
+		</div>
+		<div class="w-1/5 h-full border-black border-2">
+			<p class="text-3xl flex justify-center items-center w-full h-full">
+				{{ this.letters[2] }}
+			</p>
+		</div>
+		<div class="w-1/5 h-full border-black border-2">
+			<p class="text-3xl flex justify-center items-center w-full h-full">
+				{{ this.letters[3] }}
+			</p>
+		</div>
+		<div class="w-1/5 h-full border-black border-2">
+			<p class="text-3xl flex justify-center items-center w-full h-full">
+				{{ this.letters[4] }}
+			</p>
+		</div>
+	</div>
 </template>
 
 <script>
